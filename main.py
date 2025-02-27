b7b7f6ef308dfa6053a4ce9947c42e88ae4d723c
Daniil Dolaprolorap <dolaprolorap@DOLAPROLORAP.dolaprolorap.cc>
2025-02-27 16:52:29 +1000

    Исправлен cors + добавлены новые лимиты

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import sqlite3
+import time
 from flask import Flask, request
+from flask_cors import CORS
 from datetime import datetime, timedelta
 from utils import session_utils
 
 app = Flask(__name__)
 DB_PATH = "prod.db"
+CORS(app, origins=["null"])
 
 
 def init_db():
@@ -15,7 +18,8 @@ def init_db():
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 public_id TEXT UNIQUE,
                 steps INTEGER,
-                is_finished BOOLEAN
+                is_finished BOOLEAN,
+                last_updated INTEGER
             )
         ''')
         conn.commit()
@@ -24,6 +28,8 @@ def init_db():
 init_db()
 
 timedelta_between_create_requests = timedelta(milliseconds=10)
+ms_between_update_requests = 10
+
 last_created_session_datetime: datetime | None = None
 
 
@@ -39,11 +45,13 @@ def create_session():
     last_created_session_datetime = current_datetime
     public_id = session_utils.create_session_id()
 
+    current_timestamp_ns = time.time_ns() // 1_000_000
+
     with sqlite3.connect(DB_PATH) as conn:
         cursor = conn.cursor()
         cursor.execute('''
-            INSERT INTO sessions (public_id, steps, is_finished) VALUES (?, ?, ?)
-        ''', (public_id, 0, False))
+            INSERT INTO sessions (public_id, steps, is_finished, last_updated) VALUES (?, ?, ?, ?)
+        ''', (public_id, 0, False, current_timestamp_ns))
         conn.commit()
 
     return {'success': True, 'data': {'public_id': public_id}}
@@ -54,6 +62,9 @@ def update_session():
     data = request.get_json(force=True)
     public_id, steps, is_finished = data['public_id'], data['steps'], data['is_finished']
 
+    if steps < 0 or steps > 100:
+        return {'success': False, 'msg': 'Invalid steps count'}
+
     with sqlite3.connect(DB_PATH) as conn:
         cursor = conn.cursor()
         session_data = cursor.execute('''
@@ -63,7 +74,11 @@ def update_session():
         if session_data is None:
             return {'success': False, 'msg': 'No session with current public_id'}
 
-        _, _, current_steps, current_finished_status = session_data
+        _, _, current_steps, current_finished_status, last_updated = session_data
+
+        current_timestamp_ns = time.time_ns() // 1_000_000
+        if (current_timestamp_ns - last_updated) <= ms_between_update_requests:
+            return {'success': False, 'msg': 'Too frequent update requests'}
 
         if current_finished_status == 1:
             return {'success': False, 'msg': 'Session already finished'}
@@ -71,8 +86,8 @@ def update_session():
         new_steps, new_finished_status = current_steps + steps, is_finished
 
         cursor.execute('''
-            UPDATE sessions SET steps = ?, is_finished = ? WHERE public_id = ?
-        ''', (new_steps, new_finished_status, public_id))
+            UPDATE sessions SET steps = ?, is_finished = ?, last_updated = ? WHERE public_id = ?
+        ''', (new_steps, new_finished_status, current_timestamp_ns, public_id))
         conn.commit()
 
     return {'success': True, 'data': {}}
